4adf0c56718c60e377acc11cbef3eecd9a86dbe3
DerekPha <phansalkd3@students.rowan.edu>
2019-04-06 15:28:27 -0400

    In class assignment 1. I just changed the Name and Email Address.

---
+++ Source.cpp
@@ -0,0 +1,42 @@
+
+
+
+/***********************************************
+* Name: Derek Phansalkar
+* Email: phansalkd3@students.rowan.edu
+***********************************************
+* Purpose:
+*  This program calculates area of circle
+***********************************************/
+
+#include <iostream>
+
+using namespace std; 
+
+const  float PI = 3.14159;
+
+int main()
+{
+
+	float radius, area, circumference;
+	
+	//Display purpose of the program
+	cout << " ******************************************"<<endl;
+	cout << " This program takes in radius to compute area of circle" <<endl;
+	cout << " ******************************************\n\n";
+	
+	//Get user input
+	cout << "Please input radius: ";
+	cin >> radius;
+	
+	//Compute area to PI*radius*radius
+	area = PI* radius *radius; 
+	
+	//Display the area 
+	cout << " The area is = " << area << "\n\n";
+	cout << "Program has ended"<<endl;
+
+	system("pause");
+	
+	return 0;
+}
